3708eae7ee6e66e11ebd319451d9b2d3ac82840d
Astri_ <malo1.thiebaud@gmail.com>
2023-12-11 01:09:27 +0100

    made a WAY better algorithm

--- 2023/dayy10/d10_2_v2.py
+++ 2023/dayy10/d10_2_v2.py
@@ -10,8 +10,7 @@ def init_s():
     for y in range(len(map)):
         for x in range(len(map[y])):
             if map[y][x][0] == 'S':
-                print (x,y)
-                exit()
+                return (x,y)
             
 def move(x, y, last):
     tile = map[y][x][0]
@@ -27,15 +26,13 @@ def move(x, y, last):
         case '.': return (-1,-1,'')
 
 def travel():
-    
+    x, y = init_s()
     # look at input to guess first possible move
     if test:
-        x, y = 1, 1
-        map[y][x][1] = True
+        map[y][x][1] = ('F',True)
         dir='s'; y+=1
     else:
-        x, y = 109, 76
-        map[y][x][1] = True
+        map[y][x] = ('J',True)
         dir = 'n'; y-=1
     ###
     while(not map[y][x][1]):
@@ -45,8 +42,40 @@ def clear():
     for y in range(len(map)):
         for x in range(len(map[y])):
             if not map[y][x][1]:
-                map[y][x]="."
+                map[y][x] ="."
             else: map[y][x] = map[y][x][0]
+
+def is_inside(x, y):
+    s = 0
+    while(x > 0):
+        x-=1
+        if map[y][x] in "|F7":
+            s+=1
+    return s%2
+
+def fill(x, y, c):
+    stack = {(x,y)}
+    res = 0
+    while len(stack) != 0:
+        x, y = stack.pop()
+        if map[y][x] != '.': continue
+        map[y][x] = c
+        res+=1
+        if(x > 0): stack.add((x-1,y))
+        if(x < len(map[y])-1): stack.add((x+1,y))
+        if(y > 0): stack.add((x,y-1))
+        if(y < len(map)-1): stack.add((x,y+1))
+    return res
+
+def count_inside():
+    res = 0
+    for y in range(len(map)):
+        for x in range(len(map[y])):
+            if(map[y][x] == '.'):
+                if is_inside(x,y):
+                    res+=fill(x, y, '#')
+                else: fill(x, y, ' ')
+    print(res)
 
 def print_map():
     s = ""
@@ -54,9 +83,10 @@ def print_map():
         for t in line:
             s+=t[0]
         s+="\n"
-    with open("dayy10/cleared.txt", 'w') as f:
+    with open("dayy10/v2.txt", 'w') as f:
         f.write(s)
 
 travel()
 clear()
+count_inside()
 print_map()
