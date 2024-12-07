529fa75c2dbef23d50177a5a097cf6b7e5f10505
Astri2 <malo1.thiebaud@gmail.com>
2024-12-07 06:58:23 +0100

    small change to day6

--- 2024/d6/p2.py
+++ 2024/d6/p2.py
@@ -10,33 +10,38 @@ with open(f"2024/{os.path.split(os.path.split(__file__)[0])[1]}/input{"_test2" i
     lines = f.read().splitlines()
 
 map = [[""]*len(lines[0]) for _ in range(len(lines))]
-next_dirs = {'^': '>', '>': 'v', 'v': '<', '<': '^'}
-dir_to_int = {'^': 0, '>': 1, 'v': 2, '<': 3}
 dirs = {'^': (-1,0), '>': (0,1), 'v': (1,0), '<': (0,-1)}
+
+next_dirs = {'^': '>', '>': 'v', 'v': '<', '<': '^'}
+def turn(pos):
+    return pos[0], pos[1], next_dirs[pos[2]]
+
+def move(pos):
+    return pos[0]+dirs[pos[2]][0], pos[1]+dirs[pos[2]][1], pos[2]
+
+def move_back(pos):
+    return pos[0]-dirs[pos[2]][0], pos[1]-dirs[pos[2]][1], pos[2]
 
 def run(pos: tuple[int, int, str]) -> set[tuple[int, int]]:
     visited_pos = set()
     visited_pos.add(pos)
     while True:
 
-        dir = pos[2]
-        i_, j_ = pos[0]+dirs[dir][0], pos[1]+dirs[dir][1]
+        i_, j_, dir = move(pos)
 
         # exits the map
         if i_ < 0 or i_ == len(lines) or j_ < 0 or j_ == len(lines[0]):
             break
         elif map[i_][j_] == '#':
             # turn right
-            pos = (pos[0], pos[1], next_dirs[dir])
+            pos = turn(pos)
             if pos in visited_pos: return -1
         else: 
             pos = i_, j_, dir
             if pos in visited_pos: return -1
             visited_pos.add(pos)
 
-    # extract only location, ignore direction
-    uniq_pos = {(p[0], p[1]) for p in visited_pos}
-    return uniq_pos
+    return visited_pos
 
 def main():
     for i,line in enumerate(lines):
@@ -45,20 +50,26 @@ def main():
                 map[i][j] = '.'
                 start = (i, j, c)
             else: map[i][j] = c
-    uniq_pos = run(start)
-    assert uniq_pos != -1
+
+    uniq_pos_dir = run(start)
+    assert uniq_pos_dir != -1
+
+    # extract only location, ignore direction
+    uniq_pos = {(p[0], p[1]) for p in uniq_pos_dir}
     print("P1", len(uniq_pos))
 
     # in p2 we can't place on spawn point
     if (start[0], start[1]) in uniq_pos: uniq_pos.remove((start[0], start[1]))
 
-    legit_blocks = 0
+    legit_block_pos = set()
     for i, pos in enumerate(uniq_pos):
         i,j = pos
         map[i][j] = "#"
-        if(run(start) == -1): legit_blocks+=1
+        if(run(start) == -1): 
+            legit_block_pos.add((i,j))
         map[i][j] = "."
-    print("P2", legit_blocks)
+    if (start[0], start[1]) in legit_block_pos: legit_block_pos.remove((start[0], start[1]))
+    print("P2", len(legit_block_pos))
 
 if __name__ == "__main__":
     main()
