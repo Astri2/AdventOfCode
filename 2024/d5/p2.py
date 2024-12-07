d74c9a40b979deffa6e83cd10b17db406b806585
Astri2 <malo1.thiebaud@gmail.com>
2024-12-07 06:56:45 +0100

    fixed path

--- 2024/d5/p2.py
+++ 2024/d5/p2.py
@@ -1,6 +1,6 @@
 TEST = False
 import os
-with open(f"{os.path.split(os.path.split(__file__)[0])[1]}/input{"_test" if TEST else ""}.txt") as f:
+with open(f"2024/{os.path.split(os.path.split(__file__)[0])[1]}/input{"_test" if TEST else ""}.txt") as f:
     lines = f.read().splitlines()
 
 # snip{ topological_order_dfs
